6e1c3dc8cb3cd41d478dd4368d1eb50b52625fa6
Charles Oh <mun5424@gmail.com>
2018-03-07 23:15:23 -0800

    all functionalities added except for executeFile

---
+++ ciri.js
@@ -0,0 +1,159 @@
+require("dotenv").config();
+var keys = require('./keys.js');
+var request = require("request");
+var fs = require("fs");
+
+var parameter1 = process.argv[2];
+var parameter2 = process.argv[3]; 
+
+executeCommand(parameter1, parameter2);
+
+function executeCommand(command, argument)
+{
+    switch (command) {
+        case "my-tweets":
+            display20tweets();
+            break;
+        case "spotify-this-song":
+            var song = argument;
+            spotifySong(songname);
+            break;
+        case "movie-this":
+            var movie = argument;
+            movieInfo(movie);
+            break;
+        case "do-what-it-says":
+            executeFile();
+            break;
+        default:
+            console.log("That is not a valid command.");
+    }
+}
+
+
+function display20tweets() {
+    fs.appendFile('./log.txt', 'Command my-tweets\n', (err) => {
+        if (err)
+            throw err;
+    });
+
+    var client = new Twitter(keys.twitter);
+    var params = { screen_name: "charlesoh9", count: 20 };
+    client.get("statuses/user_timeline", params, function (error, tweets, response) {
+        if (error)
+            return;
+        else {
+            var output = "";
+            for (var i = 0; i < tweets.length; i++) {
+                output += "\n" + tweets[i].text + "\n" + "Created On: " + tweets[i].created_at;
+            }
+            fs.appendFile(".log.txt", "Twitter API Response: \n" + output + "\n", (error) => {
+                if (error)
+                    throw error;
+            });
+            console.log(output);
+        }
+    });
+}
+
+function spotifySong(song) {
+    fs.appendFile('./log.txt', 'Command spotify-this-song\n', (error) => {
+        if (error)
+            throw error;
+    });
+
+    if(song == "")
+        song = "The Sign Ace of Base";
+    
+    var spotify = new Spotify(keys.spotify);
+    spotify.search({type: "track", query: song}, function (error, response) {
+        if (error)
+            return;
+        else {
+            var output = ""; 
+            var songInfo = response.tracks.item[0]; 
+            if(songInfo == undefined)
+            {
+                console.log("No song information for this track! Try something different. ");
+            }
+            else
+            {
+                output += "Song Information: \nSong Name: " + songInfo.name + "\nArtist(s): ";
+                for(var i = 0; i < songInfo.artists.length; i++) 
+                    output += songInfo.artists[i] + ", "; 
+                output += "\nAlbum " + songInfo.album.name +"\nPreview Link: " + songInfo.preview_url + "\n"; 
+            }
+
+            fs.appendFile(".log.txt", "Spotify Response: \n" + output + "\n", (error) => {
+                if (error)
+                    throw error;
+            });
+            console.log(output);
+        }
+    });
+}
+
+function movieInfo(movie) {
+    fs.appendFile('./log.txt', 'Command movie-this\n', (error) => {
+        if (error)
+            throw error;
+    });
+
+
+    if(movie == "")
+    {   
+        movie = "Mr.Nobody"; 
+    }
+
+    request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            movieInfo = JSON.parse(body);
+            
+            if(movieInfo == undefined)
+            {
+                console.log("No movie information for this movie! Try something different. ");
+            }
+            else
+            {
+                output += "Movie Information: \nTitle: " + movieInfo.name + 
+                "\nRated: " + movieInfo.Rated + 
+                "\nIMDB Rating:" + movieInfo.imdbRating + 
+                "\nRotten Tomatoes Rating : " + movieInfo.Ratings[1].Value + 
+                "\nCountry of Origin: " + movieInfo.Country + 
+                "\nLanguage: " + movieInfo.Language +
+                "\nPlot: " + movieInfo.Plot + 
+                "\nActors: "; 
+                for(var i = 0; i < movieInfo.Actors.length; i++) 
+                    output += movieInfo.Actors[i] + ", ";
+                output += "\n";
+                
+            fs.appendFile(".log.txt", "OMDB Response: \n" + output + "\n", (error) => {
+                if (error)
+                    throw error;
+            });
+            console.log(output);
+            }
+        }
+      });
+      
+}
+
+function executeFile(file) {
+    
+    fs.appendFile('./log.txt', 'do-what-it-says\n', (error) => {
+        if (error)
+            throw error;
+    });
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+          return console.log(error);
+        }
+
+        // Then split it by commas (to make it more readable)
+        var data = data.split("\n");
+
+      });
+      
+
+}
