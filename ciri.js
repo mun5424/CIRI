f4e3732e77ebec3965f960f9568aa03f082b1c10
Charles Oh <mun5424@gmail.com>
2018-03-08 17:44:51 -0800

    All commands working

--- ciri.js
+++ ciri.js
@@ -2,21 +2,22 @@ require("dotenv").config();
 var keys = require('./keys.js');
 var request = require("request");
 var fs = require("fs");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
 var parameter1 = process.argv[2];
-var parameter2 = process.argv[3]; 
+var parameter2 = process.argv[3];
 
 executeCommand(parameter1, parameter2);
 
-function executeCommand(command, argument)
-{
+function executeCommand(command, argument) {
     switch (command) {
         case "my-tweets":
             display20tweets();
             break;
         case "spotify-this-song":
             var song = argument;
-            spotifySong(songname);
+            spotifySong(song);
             break;
         case "movie-this":
             var movie = argument;
@@ -45,7 +46,7 @@ function display20tweets() {
         else {
             var output = "";
             for (var i = 0; i < tweets.length; i++) {
-                output += "\n" + tweets[i].text + "\n" + "Created On: " + tweets[i].created_at;
+                output += "\n" + tweets[i].text + "\n" + "Created On: " + tweets[i].created_at + "\n";
             }
             fs.appendFile(".log.txt", "Twitter API Response: \n" + output + "\n", (error) => {
                 if (error)
@@ -62,26 +63,28 @@ function spotifySong(song) {
             throw error;
     });
 
-    if(song == "")
+    if (song == "")
         song = "The Sign Ace of Base";
-    
-    var spotify = new Spotify(keys.spotify);
-    spotify.search({type: "track", query: song}, function (error, response) {
+
+    var spotify = new Spotify({
+        id: keys.spotify.id,
+        secret: keys.spotify.secret
+    });
+
+    spotify.search({ type: "track", query: song }, function (error, response) {
         if (error)
             return;
         else {
-            var output = ""; 
-            var songInfo = response.tracks.item[0]; 
-            if(songInfo == undefined)
-            {
+            var output = "";
+            var songInfo = response.tracks.items[0];
+            if (songInfo == undefined) {
                 console.log("No song information for this track! Try something different. ");
             }
-            else
-            {
+            else {
                 output += "Song Information: \nSong Name: " + songInfo.name + "\nArtist(s): ";
-                for(var i = 0; i < songInfo.artists.length; i++) 
-                    output += songInfo.artists[i] + ", "; 
-                output += "\nAlbum " + songInfo.album.name +"\nPreview Link: " + songInfo.preview_url + "\n"; 
+                for (var i = 0; i < songInfo.artists.length; i++)
+                    output += songInfo.artists[i].name + ", ";
+                output += "\nAlbum: " + songInfo.album.name + "\nPreview Link: " + songInfo.preview_url + "\n";
             }
 
             fs.appendFile(".log.txt", "Spotify Response: \n" + output + "\n", (error) => {
@@ -100,60 +103,57 @@ function movieInfo(movie) {
     });
 
 
-    if(movie == "")
-    {   
-        movie = "Mr.Nobody"; 
+    if (movie == "") {
+        movie = "Mr.Nobody";
     }
 
-    request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+    request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
         if (!error && response.statusCode === 200) {
             movieInfo = JSON.parse(body);
-            
-            if(movieInfo == undefined)
-            {
+
+            if (movieInfo == undefined) {
                 console.log("No movie information for this movie! Try something different. ");
             }
-            else
-            {
-                output += "Movie Information: \nTitle: " + movieInfo.name + 
-                "\nRated: " + movieInfo.Rated + 
-                "\nIMDB Rating:" + movieInfo.imdbRating + 
-                "\nRotten Tomatoes Rating : " + movieInfo.Ratings[1].Value + 
-                "\nCountry of Origin: " + movieInfo.Country + 
-                "\nLanguage: " + movieInfo.Language +
-                "\nPlot: " + movieInfo.Plot + 
-                "\nActors: "; 
-                for(var i = 0; i < movieInfo.Actors.length; i++) 
-                    output += movieInfo.Actors[i] + ", ";
+            else {
+                var output = ""; 
+                output += "Movie Information: \nTitle: " + movieInfo.Title +
+                    "\nRated: " + movieInfo.Rated +
+                    "\nIMDB Rating:" + movieInfo.imdbRating +
+                    "\nRotten Tomatoes Rating : " + movieInfo.Ratings[1].Value +
+                    "\nCountry of Origin: " + movieInfo.Country +
+                    "\nLanguage: " + movieInfo.Language +
+                    "\nPlot: " + movieInfo.Plot +
+                    "\nActors: " + movieInfo.Actors;
                 output += "\n";
-                
-            fs.appendFile(".log.txt", "OMDB Response: \n" + output + "\n", (error) => {
-                if (error)
-                    throw error;
-            });
-            console.log(output);
+
+                fs.appendFile(".log.txt", "OMDB Response: \n" + output + "\n", (error) => {
+                    if (error)
+                        throw error;
+                });
+                console.log(output);
             }
         }
-      });
-      
+    });
+
 }
 
 function executeFile(file) {
-    
+
     fs.appendFile('./log.txt', 'do-what-it-says\n', (error) => {
         if (error)
             throw error;
     });
 
-    fs.readFile("random.txt", "utf8", function(error, data) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
-          return console.log(error);
+            return console.log(error);
         }
 
-        // Then split it by commas (to make it more readable)
-        var data = data.split("\n");
+        var data = data.split(",");
+        //could potentially turn it to something where it can do any command, not just spotify
+        spotifySong(data[1]);
 
-      });
-      
+    });
+
 
 }
